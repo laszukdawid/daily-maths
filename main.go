247a7f1cb1f008a2c1fd6166c7a95cc3642c6aa0
Dawid Laszuk <laszukdawid@gmail.com>
2017-04-08 14:43:40 +0200

    Initial commit. See README.md or go through files.

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "os"
+    "math/rand"
+    //"strconv"
+    "time"
+)
+
+type Config struct {
+    User    string
+    Level   string
+    Num     int
+}
+
+func save_result(score int, config Config) (){
+    path := "./results/"+config.User
+
+    // If no score for given user
+    if _, err := os.Stat(path); os.IsNotExist(err) {
+        fmt.Println("Creting score file for user", config.User)
+        f, err := os.Create(path)
+        if err != nil {
+            panic(err)
+        }
+        f.Close()
+    }
+    // File exists, just open it
+    f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0600)
+    if err != nil {
+        panic(err)
+    }
+
+    defer f.Close()
+
+    data_str := time.Now().Format("2006-01-02 15:04:05")
+    level := config.Level[0:1]
+    text := fmt.Sprintf("%s %d %s\n", data_str, score, level)
+    if _, err = f.WriteString(text); err != nil {
+        panic(err)
+    }
+}
+
+func get_random_values(level string) (float32, float32){
+    seed := rand.NewSource(time.Now().UnixNano())
+    randgen := rand.New(seed)
+    var r1, r2 float32
+    switch level {
+    case "Easy":
+        r1 = float32(randgen.Intn(20))/2
+        r2 = float32(randgen.Intn(20))/2
+    case "Normal":
+        r1 = float32(randgen.Intn(100)-50)/2
+        r2 = float32(randgen.Intn(100)-50)/2
+    case "Hard":
+        r1 = float32(randgen.Intn(200)-100)/8
+        r2 = float32(randgen.Intn(200)-100)/8
+    default:
+        panic("DEFINE YOURSELF!")
+    }
+    return r1, r2
+}
+
+func main() {
+    file, _ := os.Open("config.json")
+    decoder := json.NewDecoder(file)
+    config := Config{}
+
+    err := decoder.Decode(&config)
+    if err != nil {
+        fmt.Println("Error while decoding config", err)
+    }
+
+    fmt.Printf("Hello, %s. Let's do this!\n", config.User)
+    fmt.Printf("Selected difficulty: %s\n", config.Level)
+
+    var r1, r2 float32
+
+    // Variables
+    var answer, expected float32
+    score := 0
+    good_responses := []string{"Awesome.", "Obviously.", "Yep.",
+        "10 points for "+config.User+"dor.",
+        "Whoever thinks otherwise is stupid."}
+
+    for i:=0; i<config.Num; i++ {
+        fmt.Printf("%d. ", i)
+
+        // TODO: Why only two variables?
+        r1, r2 = get_random_values(config.Level)
+        expected = r1 + r2
+
+        // TODO: Surely other operations as well
+        fmt.Printf("%g + %g = ", r1, r2)
+        _, err := fmt.Scanf("%g", &answer)
+        if err != nil {
+            fmt.Println("Cannot read answer:", err)
+        }
+
+        if answer==expected {
+            response_idx := rand.Intn(len(good_responses))
+            fmt.Println(good_responses[response_idx])
+            score += 1
+        } else {
+            fmt.Println("Nope. Excpected:", expected)
+        }
+    }
+    fmt.Printf("Final score: %d/%d", score, config.Num)
+
+    // Saving 
+    save_result(score, config)
+
+}
+
